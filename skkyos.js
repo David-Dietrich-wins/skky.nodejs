1706c767a73fe70a7197904991fdf444b290cf7a
David Dietrich <ddietrich@intecore.com>
2025-09-14 13:30:00 -0400

    fix: removed legacy npm packages

--- skkyos.js
+++ skkyos.js
@@ -1,196 +1,187 @@
-const fs = require('fs');
-const os = require('os');
+const fs = require('node:fs')
+const os = require('node:os')
 
-const skky = require('./skky');
+const skky = require('./skky')
 
 module.exports = {
-	osinfo: null,
+  osinfo: null,
 
-	getHostname: function() {
-		return this.getOsInfo().osHostname;
-	},
-	getHostnameClean: function() {
-		return skky.nonNull(this.getHostname().replace(/[^\x20-\x7E]+/g, ''));
-	},
-	getUsername: function() {
-		return this.getOsInfo().userinfo.username;
-	},
-	getConfig: function(filename) {
-		if(skky.isNullOrUndefined(filename))
-			filename = skky.getConstant('ConfigFilename');
+  getHostname: function () {
+    return this.getOsInfo().osHostname
+  },
+  getHostnameClean: function () {
+    return skky.nonNull(this.getHostname().replace(/[^\x20-\x7E]+/g, ''))
+  },
+  getUsername: function () {
+    return this.getOsInfo().userinfo.username
+  },
+  getConfig: function (filename) {
+    if (skky.isNullOrUndefined(filename))
+      filename = skky.getConstant('ConfigFilename')
 
-		return JSON.parse(this.readTextFileSync(filename));
-	},
+    return JSON.parse(this.readTextFileSync(filename))
+  },
 
-	fileExistsSync: function(filename, deleteIfExists) {
-		try {
-			fs.statSync(filename);
+  fileExistsSync: function (filename, deleteIfExists) {
+    try {
+      fs.statSync(filename)
 
-			if (deleteIfExists)
-				fs.unlinkSync(filename);
-		}
-		catch(err) {
-			if(err.code == 'ENOENT') {
-				console.log(filename + ' does not exist. Skipping delete.');
-				return false;
-			}
-		}
+      if (deleteIfExists) fs.unlinkSync(filename)
+    } catch (err) {
+      if (err.code == 'ENOENT') {
+        console.log(filename + ' does not exist. Skipping delete.')
+        return false
+      }
+    }
 
-		return true;
-	},
+    return true
+  },
 
-	saveJsonToFileSync: function(jo, filename) {
-		const fname = 'saveJsonToFileSync:';
-		var backupFilename = filename + '.bak';
+  saveJsonToFileSync: function (jo, filename) {
+    const fname = 'saveJsonToFileSync:'
+    var backupFilename = filename + '.bak'
 
-		try {
-			if(this.fileExistsSync(filename))
-				fs.writeFileSync(backupFilename, fs.readFileSync(filename));
+    try {
+      if (this.fileExistsSync(filename))
+        fs.writeFileSync(backupFilename, fs.readFileSync(filename))
 
-			fs.writeFileSync(filename, JSON.stringify(jo));
+      fs.writeFileSync(filename, JSON.stringify(jo))
 
-			var stats = fs.statSync(filename);
-			var fileSizeInBytes = stats.size;
+      var stats = fs.statSync(filename)
+      var fileSizeInBytes = stats.size
 
-			console.log(filename + ' is ' + fileSizeInBytes + ' bytes.');
-			return fileSizeInBytes;
-		}
-		catch(e) {
-			console.error(fname, e);
-		}
+      console.log(filename + ' is ' + fileSizeInBytes + ' bytes.')
+      return fileSizeInBytes
+    } catch (e) {
+      console.error(fname, e)
+    }
 
-		return -1;
-	},
+    return -1
+  },
 
-	readTextFileSync: function(filename, replaceLineEnds) {
-		const fname = 'readTextFileSync: ';
+  readTextFileSync: function (filename, replaceLineEnds) {
+    const fname = 'readTextFileSync: '
 
-		var s = '';
-		try {
-			if (fs.existsSync(filename)) {
-				s = fs.readFileSync(filename, 'utf8');
-				if(replaceLineEnds)
-					s = skky.nonNull(s).replace(/\n$/, '');
-			}
-			else {
-				console.log(fname, filename, ' does not exist.');
-			}
-		}
-		catch(err) {
-			console.log(fname, 'Error reading', skky.nonNull(filename), 'file.');
-			console.error(fname, err);
-		}
+    var s = ''
+    try {
+      if (fs.existsSync(filename)) {
+        s = fs.readFileSync(filename, 'utf8')
+        if (replaceLineEnds) s = skky.nonNull(s).replace(/\n$/, '')
+      } else {
+        console.log(fname, filename, ' does not exist.')
+      }
+    } catch (err) {
+      console.log(fname, 'Error reading', skky.nonNull(filename), 'file.')
+      console.error(fname, err)
+    }
 
-		return skky.nonNull(s);
-	},
+    return skky.nonNull(s)
+  },
 
-	getOsInfo: function() {
-		if(skky.isNullOrUndefined(this.osinfo))
-			this.osinfo = this.getOsInfoRaw();
+  getOsInfo: function () {
+    if (skky.isNullOrUndefined(this.osinfo)) this.osinfo = this.getOsInfoRaw()
 
-		return this.osinfo;
-	},
-	getOsInfoRaw: function() {
-		const fname = 'getOsInfoRaw:';
+    return this.osinfo
+  },
+  getOsInfoRaw: function () {
+    const fname = 'getOsInfoRaw:'
 
-		var osinfo = {};
+    var osinfo = {}
 
-		osinfo.cpus = os.cpus();
-		osinfo.type = os.type();
-		osinfo.architecture = os.arch();
-		osinfo.nics = os.networkInterfaces();
-		osinfo.tempDir = os.tmpdir();
-		osinfo.totalMemory = os.totalmem();
-		osinfo.freeMemory = os.freemem();
-		osinfo.uptime = os.uptime();
-		osinfo.hostname = os.hostname();
-		osinfo.platform = os.platform();
-		osinfo.release = os.release();
-		osinfo.isArduino = false;
-		osinfo.isFedora = false;
-		osinfo.isLinux = (os.platform() == 'linux');
-		osinfo.isMac = (os.platform() == 'darwin');
-		osinfo.isRaspberryPi = false;
-		osinfo.isWindows = (os.platform() == 'win32');
+    osinfo.cpus = os.cpus()
+    osinfo.type = os.type()
+    osinfo.architecture = os.arch()
+    osinfo.nics = os.networkInterfaces()
+    osinfo.tempDir = os.tmpdir()
+    osinfo.totalMemory = os.totalmem()
+    osinfo.freeMemory = os.freemem()
+    osinfo.uptime = os.uptime()
+    osinfo.hostname = os.hostname()
+    osinfo.platform = os.platform()
+    osinfo.release = os.release()
+    osinfo.isArduino = false
+    osinfo.isFedora = false
+    osinfo.isLinux = os.platform() == 'linux'
+    osinfo.isMac = os.platform() == 'darwin'
+    osinfo.isRaspberryPi = false
+    osinfo.isWindows = os.platform() == 'win32'
 
-		osinfo.userinfo = os.userInfo();
-		osinfo.ips = this.getIpAddressInfo();
+    osinfo.userinfo = os.userInfo()
+    osinfo.ips = this.getIpAddressInfo()
 
-		var distro = this.readTextFileSync('/etc/os-release');
-		if (distro.length > 0) {
-			osinfo.distro = distro;
-			if(distro.toLowerCase().indexOf('raspbian') > -1)
-				osinfo.isRaspberryPi = true;
-			if(distro.toLowerCase().indexOf('fedora') > -1)
-				osinfo.isFedora = true;
-		}
+    var distro = this.readTextFileSync('/etc/os-release')
+    if (distro.length > 0) {
+      osinfo.distro = distro
+      if (distro.toLowerCase().indexOf('raspbian') > -1)
+        osinfo.isRaspberryPi = true
+      if (distro.toLowerCase().indexOf('fedora') > -1) osinfo.isFedora = true
+    }
 
-		distro = this.readTextFileSync('/etc/release');
-		if (distro.length > 0 && distro.toLowerCase().indexOf('edison') > -1) {
-			osinfo.distro = distro;
-			osinfo.isArduino = true;
-		}
+    distro = this.readTextFileSync('/etc/release')
+    if (distro.length > 0 && distro.toLowerCase().indexOf('edison') > -1) {
+      osinfo.distro = distro
+      osinfo.isArduino = true
+    }
 
-		try {
-			osinfo.machineId = this.readTextFileSync('/etc/machine-id', true);
-		}
-		catch(err) {
-			console.log(fname, 'Error reading /etc/machine-id file on Linux.');
-			console.error(fname, err);
-		}
+    try {
+      osinfo.machineId = this.readTextFileSync('/etc/machine-id', true)
+    } catch (err) {
+      console.log(fname, 'Error reading /etc/machine-id file on Linux.')
+      console.error(fname, err)
+    }
 
-		return osinfo;
-	},
+    return osinfo
+  },
 
-	updateConfigSettings: function() {
-		var isok = true;
-		var filesize = 0;
+  updateConfigSettings: function () {
+    var isok = true
+    var filesize = 0
 
-		config.lastInitialization = skky.iot.getBase(isok ? 0 : -1);
+    config.lastInitialization = skky.iot.getBase(isok ? 0 : -1)
 
-		filesize = this.saveJsonToFileSync(config, skky.constants.ConfigFilenameSafeMode);
-		if (filesize > 100)
-			isok = true;
+    filesize = this.saveJsonToFileSync(
+      config,
+      skky.constants.ConfigFilenameSafeMode
+    )
+    if (filesize > 100) isok = true
 
-		config.osInfo = this.getOsInfo();
+    config.osInfo = this.getOsInfo()
 
-		isok = false;
-		filesize = this.saveJsonToFileSync(config, skky.constants.ConfigFilename);
-		if (filesize > 100)
-			isok = true;
+    isok = false
+    filesize = this.saveJsonToFileSync(config, skky.constants.ConfigFilename)
+    if (filesize > 100) isok = true
 
-		return isok;
-	},
+    return isok
+  },
 
-	getIpAddressInfo: function() {
-		const fname = 'getIpAddressInfo: ';
+  getIpAddressInfo: function () {
+    const fname = 'getIpAddressInfo: '
 
-		var ifaces = os.networkInterfaces();
-		var ipAddress = [];
+    var ifaces = os.networkInterfaces()
+    var ipAddress = []
 
-		Object.keys(ifaces).forEach(function (ifname) {
-			var alias = 0;
+    Object.keys(ifaces).forEach(function (ifname) {
+      var alias = 0
 
-			ifaces[ifname].forEach(function (iface) {
-				if ('IPv4' !== iface.family || iface.internal !== false) {
-					// skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
-					return;
-				}
+      ifaces[ifname].forEach(function (iface) {
+        if ('IPv4' !== iface.family || iface.internal !== false) {
+          // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+          return
+        }
 
-				if (alias >= 1) {
-					// this single interface has multiple ipv4 addresses
-					console.log(fname, ifname + ':', alias, iface.address);
-				} else {
-					// this interface has only one ipv4 adress
-					console.log(fname, ifname, iface.address);
-				}
+        if (alias >= 1) {
+          // this single interface has multiple ipv4 addresses
+          console.log(fname, ifname + ':', alias, iface.address)
+        } else {
+          // this interface has only one ipv4 adress
+          console.log(fname, ifname, iface.address)
+        }
 
-				++alias;
-				ipAddress.push(iface.address);
-			});
-		});
+        ++alias
+        ipAddress.push(iface.address)
+      })
+    })
 
-		return ipAddress;
-	}
-};
-
+    return ipAddress
+  },
+}
